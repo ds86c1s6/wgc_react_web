cbc2395b50a6aacc1aa42f17d9c20343c82b1c11
王国成 <YC01786@mbcoffice.com>
2023-02-23 13:55:15 +0800

    网页新消息闪烁

---
+++ src/core/message-blink.ts
@@ -0,0 +1,30 @@
+/**
+ * 网页新消息闪烁功能
+ *
+ * 启动：message.start(msg)
+ * 关闭：message.stop();
+ */
+
+export const message = {
+  timer: null,
+  oldTitle: document.title,
+  time: 0,
+  // 开始闪烁
+  start: (msg, intervalTime = 600) => {
+    message.timer = setInterval(() => {
+      message.time++;
+      let title = "";
+      if (message.time % 2 === 0) {
+        title = message.oldTitle;
+      } else {
+        title = msg;
+      }
+      document.title = title;
+    }, intervalTime);
+  },
+  // 结束闪烁
+  stop: () => {
+    document.title = message.oldTitle;
+    clearInterval(message.timer);
+  },
+};

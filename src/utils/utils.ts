20ad43506eb5c9e97c7aa4937e21a8aa20615639
王国成 <YC01786@mbcoffice.com>
2023-05-12 17:13:40 +0800

    await错误捕获工具函数

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,25 @@ export const desensitizePhone = (phone: string) => {
   const reg = /(\d{3})\d{4}(\d{4})/;
   return phone.replace(reg, "$1****$2");
 };
+
+/**
+ *
+ * @param promise
+ * @param errorText
+ * @returns
+ * const [err, data] = await to(getUserInfo());
+ */
+export const to = <T, U = Error>(
+  promise: Promise<T>,
+  errorText?: object
+): Promise<[U, undefined] | [null, T]> => {
+  return promise
+    .then<[null, T]>((data: T) => [null, data])
+    .catch<[U, undefined]>((err: U) => {
+      if (errorText) {
+        const parsedError = Object.assign({}, err, errorText);
+        return [parsedError, undefined];
+      }
+      return [err, undefined];
+    });
+};
